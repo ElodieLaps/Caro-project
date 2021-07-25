e6fa1f473c620b25a15fb7209989cc05bbcf73b3
ElodieIdaLaps <59149456+ElodieLaps@users.noreply.github.com>
2021-07-25 14:34:24 +0200

    refact character factory

--- lib/factories/character.ts
+++ lib/factories/character.ts
@@ -9,18 +9,14 @@ export const createCharacters = (
   const charactersList: Array<Character> = [];
 
   characters.forEach((character: any) => {
-    //get character race from race list
     const race = races.find((race) => race.name === character.race) as Race;
 
-    //get statistics from race
-    const raceStatistics = race ? (race.statistics as Array<Statistic>) : [];
-
+    const raceStatistics = race.statistics as Array<Statistic>;
     const statistics = [] as Array<Statistic>;
     raceStatistics.forEach((stat) => {
       statistics.push(stat);
     });
 
-    //get current value if current value in data for stat bars
     const healthDataStat =
       character.statistics &&
       character.statistics.find((stat: Statistic) => stat.type === "health");
@@ -33,7 +29,6 @@ export const createCharacters = (
         (stat: Statistic) => stat.type === "experience"
       );
 
-    //create a new character
     const newCharacter = new Character(
       character.id,
       character.name,
@@ -44,20 +39,6 @@ export const createCharacters = (
       statistics
     );
 
-    //get race stat default value for stat bars to set current
-    const healthRaceStat =
-      newCharacter.race.statistics &&
-      newCharacter.race.statistics.find(
-        (stat: Statistic) => stat.type === "health"
-      );
-    const manaRaceStat =
-      newCharacter.race.statistics &&
-      newCharacter.race.statistics.find(
-        (stat: Statistic) => stat.type === "mana"
-      );
-
-    //create or update statistics bar
-    //Experience
     const newExperienceStat = newCharacter.statistics.find(
       (stat) => stat.type === "experience"
     );
@@ -72,38 +53,39 @@ export const createCharacters = (
       experience = { ...experience, current: experienceDataStat.current };
     }
 
-    //Health
     let newHealthStat = newCharacter.statistics.find(
       (stat) => stat.type === "health"
     );
-    //create current value and set
+    const healthRaceStat =
+      newCharacter.race.statistics &&
+      newCharacter.race.statistics.find(
+        (stat: Statistic) => stat.type === "health"
+      );
     const updatedHealth = {
       ...newHealthStat,
       current: healthDataStat ? healthDataStat.current : healthRaceStat?.value,
     } as Statistic;
 
-    //Mana
     let newManaStat = newCharacter.statistics.find(
       (stat) => stat.type === "mana"
     );
-    //create current value and set
+    const manaRaceStat =
+      newCharacter.race.statistics &&
+      newCharacter.race.statistics.find(
+        (stat: Statistic) => stat.type === "mana"
+      );
     const updatedMana = {
       ...newManaStat,
       current: manaDataStat ? manaDataStat.current : manaRaceStat?.value,
     } as Statistic;
 
-    //put all in statistic array
     if (!newExperienceStat) {
       newCharacter.statistics.unshift(experience);
     }
     newCharacter.statistics.splice(1, 2, updatedHealth, updatedMana);
 
-    //update statistics
     newCharacter.updateStatisticsWithLevel();
-
     console.log("newCharacter", newCharacter);
-
-    //put new character in characters list
     charactersList.push(newCharacter);
   });
   return charactersList;
